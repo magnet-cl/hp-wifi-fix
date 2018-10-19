9937b693a0f3c37fd2f8bf0208267085e392deb3
Mauricio Castro <mauricio@wooo.cl>
2018-10-19 19:18:56 -0300

    Update to replace new Ubuntu drivers

--- fix.sh
+++ fix.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
+# Clone and install the latest drivers
+# This is required since ubuntu drivers stopped accepting the ant_sel option.
+sudo apt-get install linux-headers-generic build-essential
+cd ..
+git clone https://github.com/magnet-cl/rtlwifi_new
+cd rtlwifi_new
+make
+sudo make install
+cd ..
+rm -rf rtlwifi_new
+
+
 # Persistent solution
 # Available antennas: 1 and 2. Default value is 0 (auto)
-# For the HP250G4 the physical antenna is connected to the first connector (ant_sel=1)
+# For the HP250G4 the physical antenna is connected to the second connector (ant_sel=2)
 # The second parameter disables firmware power saving to avoid random disconnections (fwlps=0)
-echo "options rtl8723be ant_sel=1 fwlps=0" | sudo tee /etc/modprobe.d/rtl8723be.conf
+echo "options rtl8723be ant_sel=2 fwlps=0" | sudo tee /etc/modprobe.d/rtl8723be.conf
 
-# Apply the patch now, without reboot
+# Apply the patch now, without reboot, unload existing module
 sudo modprobe -r rtl8723be
-sudo modprobe rtl8723be ant_sel=1
+# Load the new one
+sudo modprobe rtl8723be ant_sel=2
 
 echo "Done!"
 
